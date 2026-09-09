4d776e9b80b71998a2f63eab7d837f5beeaa1b0c
Agustin Bianco <bianco@MacBook-Air-de-Agustin.local>
2026-09-09 20:04:03 -0300

    Primer commit

---
+++ src/main/java/com/example/sumaunplato/SumaUnPlatoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sumaunplato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumaUnPlatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumaUnPlatoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sumaunplato/SumaUnPlatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sumaunplato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumaUnPlatoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
